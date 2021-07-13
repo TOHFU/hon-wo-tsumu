2c83d453f260e230fc0616e08fe11e553a8a4fad
tohfu <87093hayato@gmail.com>
2021-07-13 11:21:18 +0900

    init native-base

--- HonWoTsumu/App.tsx
+++ HonWoTsumu/App.tsx
@@ -9,107 +9,71 @@
  */
 
 import React from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  NativeBaseProvider,
+  Box,
   Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  HStack,
+  Center,
+  Pressable,
+} from 'native-base';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  const [selected, setSelected] = React.useState(1);
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <Box flex={1} bg="white" safeAreaTop>
+        <Center flex={1} />
+        <HStack bg="#fff" alignItems="center" safeAreaBottom shadow={6}>
+          <Pressable
+            opacity={selected === 0 ? 1 : 0.5}
+            py={2}
+            flex={1}
+            onPress={() => setSelected(0)}>
+            <Center>
+              <Icon name="book" size={30} color="#333" />
+              <Text marginTop="2" color="#333" fontSize={14}>
+                よむ
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable
+            opacity={selected === 1 ? 1 : 0.5}
+            py={2}
+            flex={1}
+            onPress={() => setSelected(1)}>
+            <Center>
+              <Icon name="seedling" size={30} color="#333" />
+              <Text marginTop="2" color="#333" fontSize={14}>
+                つむ
+              </Text>
+            </Center>
+          </Pressable>
+          <Pressable
+            opacity={selected === 2 ? 1 : 0.5}
+            py={2}
+            flex={1}
+            onPress={() => setSelected(2)}>
+            <Center>
+              <Icon name="cat" size={30} color="#333" />
+              <Text marginTop="2" color="#333" fontSize={14}>
+                猫である
+              </Text>
+            </Center>
+          </Pressable>
+        </HStack>
+      </Box>
+    </NativeBaseProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
